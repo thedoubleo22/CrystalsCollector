7fc5f6f4ffc6a95569c6bbe489153cf605aae267
Olivia Oxley <oliviaoxley@olivias-macbook.lan>
2018-10-13 16:48:48 -0700

    I almost had a heart attack when the javascript decided to work:(

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,60 @@
-/* Psuedo Code Outline
-I. Var needed
-- win, loss, blue crystal, red crystal, pink crystal, purple crystal
-II. Functions
-III. 
+//Initialize Variables for Crystals, Win/Loss, 
+var gemOne;
+var gemTwo;
+var gemThree;
+var gemFour;
+
+var targetNumber;
+var totalScore = 0;
+var winScore = 0;
+var lossScore = 0;
+
+//Reset Function
+function reset() {
+    targetNumber = Math.floor(Math.random() * 50) + 15;
+    gemOne = Math.floor(Math.random() * 10) + 1;
+    gemTwo = Math.floor(Math.random() * 10) + 1;
+    gemThree = Math.floor(Math.random() * 10) + 1;
+    gemFour = Math.floor(Math.random() * 10) + 1;
+    totalScore = 0;
+    $("#wins").text(winScore)
+    $("#loss").text(lossScore)
+    $("#total").text(totalScore)
+    $("#random").text(targetNumber);
+}
+
+console.log(reset)
+//Call Back Functions
+$(document).ready(function () {
+
+    $("#green").on("click", function() {
+        Score(gemOne);
+    });
+
+    $("#lilac").on("click", function() {
+        Score(gemTwo);
+    });
+
+    $("#pink").on("click", function() {
+        Score(gemThree);
+    });
+
+    $("#blue").on("click", function() {
+        Score(gemFour);
+    });
+
+//Events (Score Function)
+    function Score(gemAmount) {
+        totalScore += gemAmount
+        $("#total").text(totalScore)
+        if (totalScore === targetNumber) {
+            winCount++;
+            reset();
+            }
+        else if (totalScore >= targetNumber) {
+            lossScore++;
+            reset();
+        };
+    }
+    reset();
+});
